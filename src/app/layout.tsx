51791150a2659fac0067356e96f53f8a78eb3465
Manuel Ortega <mortega@apgca.cl>
2025-09-12 17:48:47 -0300

    remove header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
               <HpHeader />  */}
                 {/* {children} */}
                 <div className='isolate'>{children}</div>
-                <Footer />
+                {/* <Footer /> */}
                 <ScrollToTop />
               {/* </PageContainer> */}
             </MyApp>
